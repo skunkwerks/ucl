e1146722b8bcb0a05df430ce42a6ec296ed65188
Dave Cottlehuber <dch@skunkwerks.at>
2019-12-19 20:20:24 +0000

    clean up memory allocations

--- c_src/ucl_nif.c
+++ c_src/ucl_nif.c
@@ -11,67 +11,75 @@
 #include "erl_nif.h"
 #include "ucl.h"
 
-    static ERL_NIF_TERM
-to_json(ErlNifEnv *env, int argc, const ERL_NIF_TERM argv[])
-{
-    // buffer for returning parsed JSON
-    unsigned char* buf;
-    ERL_NIF_TERM json;
+static ERL_NIF_TERM to_json(ErlNifEnv *env, int argc,
+                            const ERL_NIF_TERM argv[]) {
+  // ucl bits and pieces
+  struct ucl_parser *parser = NULL;
 
-    // ucl bits and pieces
-    struct ucl_parser *parser = NULL;
+  ucl_object_t *ucl = NULL;
+  ucl_emitter_t emitter = UCL_EMIT_JSON;
 
-    ucl_object_t *ucl = NULL;
-    ucl_emitter_t emitter = UCL_EMIT_JSON;
+  if (!enif_is_binary(env, argv[0])) {
+    ERL_NIF_TERM error = enif_make_atom(env, "error");
+    ERL_NIF_TERM reason = enif_make_atom(env, "badarg");
+    return enif_make_tuple2(env, error, reason);
+  }
 
-    if (!enif_is_binary(env, argv[0])) {
-        ERL_NIF_TERM error = enif_make_atom(env, "error");
-        ERL_NIF_TERM reason = enif_make_atom(env, "badarg");
-        return enif_make_tuple2(env, error, reason);
-    }
+  ErlNifBinary binary;
+  enif_inspect_binary(env, argv[0], &binary);
 
-    ErlNifBinary binary;
-    enif_inspect_binary(env, argv[0], &binary);
+  // parse UCL
+  // https://github.com/vstakhov/libucl/blob/master/doc/api.md#parser-functions-1
+  parser = ucl_parser_new(UCL_PARSER_KEY_LOWERCASE);
+  ucl_parser_add_chunk(parser, binary.data, binary.size);
 
-    // TODO ensure binary is NULL terminated
+  // does it even parse correctly
+  if (ucl_parser_get_error(parser)) {
+    ERL_NIF_TERM error = enif_make_atom(env, "error");
+    ERL_NIF_TERM reason = enif_make_atom(env, "ucl_invalid");
+    return enif_make_tuple2(env, error, reason);
+  }
 
-    // parse UCL
-    // https://github.com/vstakhov/libucl/blob/master/doc/api.md#parser-functions-1
-    parser = ucl_parser_new(UCL_PARSER_KEY_LOWERCASE);
-    ucl_parser_add_chunk (parser, binary.data, binary.size);
+  ucl = ucl_parser_get_object(parser);
 
-    if (ucl_parser_get_error (parser)) {
-        ERL_NIF_TERM error = enif_make_atom(env, "error");
-        ERL_NIF_TERM reason = enif_make_atom(env, "ucl_invalid");
-        return enif_make_tuple2(env, error, reason);
-    }
-    else if ((ucl = ucl_parser_get_object(parser)) == NULL) {
-        ERL_NIF_TERM error = enif_make_atom(env, "error");
-        ERL_NIF_TERM reason = enif_make_atom(env, "ucl_no_root");
-        return enif_make_tuple2(env, error, reason);
-    }
-    else {
-        ucl = ucl_parser_get_object (parser);
-    }
+  // there should always be a root object to generate a tree from
+  if (ucl == NULL) {
+    ERL_NIF_TERM error = enif_make_atom(env, "error");
+    ERL_NIF_TERM reason = enif_make_atom(env, "ucl_no_root");
+    return enif_make_tuple2(env, error, reason);
+  }
 
-    if (parser != NULL) {
-        ucl_parser_free (parser);
-    }
-    if (ucl != NULL) {
-        ucl_object_unref (ucl);
-    }
+  // finished with parser
+  if (parser != NULL) {
+    ucl_parser_free(parser);
+  }
 
-    // turn it into JSON
-    size_t len;
-    unsigned char *blob;
-    blob = ucl_object_emit_len(ucl, emitter, &len);
+  // turn it into JSON
+  size_t len;
+  unsigned char *blob;
+  blob = ucl_object_emit_len(ucl, emitter, &len);
 
-    // return JSON as binary
-    ERL_NIF_TERM ok = enif_make_atom(env, "ok");
-    buf = enif_make_new_binary(env, len, &json);
-    memcpy(buf, blob, len);
+  // finished with UCL
+  if (ucl != NULL) {
+    ucl_object_unref(ucl);
+  }
 
-    return enif_make_tuple2(env, ok, json);
+  // buffer for returning parsed JSON
+  unsigned char *buf;
+  ERL_NIF_TERM json;
+
+  // return JSON as binary
+  ERL_NIF_TERM ok = enif_make_atom(env, "ok");
+  buf = enif_make_new_binary(env, len, &json);
+  memcpy(buf, blob, len);
+
+  // free JSON blob
+  free(blob);
+
+  return enif_make_tuple2(env, ok, json);
+}
+
+static ErlNifFunc nif_funcs[] = {{"to_json", 1, to_json}};
 
 // Public Domain code from https://github.com/davisp/nif-examples
 // There are three functions that may be called during the lifetime
