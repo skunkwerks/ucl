76ae7697bb445b94560ab3968da9b74c2d66d05a
Dave Cottlehuber <dch@skunkwerks.at>
2019-12-19 20:17:06 +0000

    tests

--- test/ucl_SUITE.erl
+++ test/ucl_SUITE.erl
@@ -6,9 +6,19 @@
 all() -> [start].
 
 start(_Config) ->
-    Simple = << "simple: true" >>,
-    ?assertEqual({ok,<<"{\n    \"simple\": true\n}">>},
-                 ucl:to_json(Simple)),
+
+    Empty = ucl:to_json(<< "" >>),
+    ?assertEqual({ok,<<"{\n\n}">>}, Empty),
+
+    Simple = ucl:to_json(<< "simple: true" >>),
+    ?assertEqual({ok,<<"{\n    \"simple\": true\n}">>}, Simple),
+
+    BadArg = ucl:to_json(error),
+    ?assertEqual({error, badarg}, BadArg),
+
+    Invalid = ucl:to_json(<< "invalid" >>),
+    ?assertEqual({error, ucl_invalid}, Invalid),
+
     %% blob taken from parent libucl project
     {ok, BlobUcl} = file:read_file("../../../../test/fixtures/blob.ucl"),
     BlobJson = file:read_file("../../../../test/fixtures/blob.json"),
